2a9cbaccbdbc1bc1182127f4b8a65f75376ae40d
Pete Davis <oldmanplanet@gmail.com>
2020-09-07 22:06:51 +0100

    Initial veg

---
+++ log.js
@@ -0,0 +1,34 @@
+const myData = {
+    year2020: {
+        week36: {
+            variety: [
+                "Avo",
+                "Bea",
+                "Cab",
+                "Car",
+                "Cuc",
+                "Gar",
+                "GrB",
+                "Lee",
+                "Mus",
+                "Oni",
+                "Pea",
+                "Tom",
+            ], // 12
+            median: 7,
+            max: 9,
+            dailyLog: year2020week36,
+        },
+    },
+};
+
+const year2020week36 = {
+    20200902: ["Avo", "Cab", "Car", "Cuc", "Gar", "GrB", "Lee", "Oni", "Tom"], // 9
+    20200903: ["Cab", "Gar", "GrB", "Oni", "Tom"], // 5
+    20200904: ["Bea", "Cab", "Cuc", "Gar", "Oni", "Tom"], // 6
+    20200905: ["Bea", "Cab", "Car", "Gar", "Oni", "Pea", "Tom"], // 7
+    20200906: ["Bea", "Cab", "Car", "Gar", "Grb", "Mus", "Oni", "Pot", "Tom"], // 9
+};
+const year2020week37 = {
+    20200907: ["Bea", "Cab", "Car", "Cou", "Gar", "Oni", "Pea", "Tom"], // 8
+};
---
+++ veg.js
@@ -0,0 +1,90 @@
+export const veg = [
+    {
+        id: 'Art',
+        names: ["Artichoke", "Globe artichoke"],
+        colors: {},
+    },
+    {
+        id: 'Aru',
+        names: ['Arugula', 'Rocket'],
+    },
+    {
+        id: 'Asp',
+        names: ["Asparagus"],
+        colors: {}
+    },
+    {
+        id: 'Avo',
+        names: ["Avocado", "Avocado pear"],
+        colors: { light: "lime", dark: "green" },
+        week: 13
+    },
+    {
+        id: 'BaS',
+        names: ["Bamboo shoots"]
+    },
+    {
+        id: 'BeS',
+        names: ["Bean sprouts", "Sprouted beans", "Mung bean sprouts"],
+        colors: {}
+    },
+    {
+        id: 'Bea',
+        names: ["Beans", 'Lentils', 'Chickpeas', 'Pulses', 'Legumes', 'Kidney beans', 'Butter beans', 'Haricot beans', 'Black beans']
+    },
+    {
+        id: 'Bee',
+        names: ["Beets", "Beetroot"]
+    },
+    {
+        id: 'Bro',
+        names: ['Broccoli', 'Tender stem broccoli', 'Purple sprouting broccoli']
+    },
+    {
+        id: 'EgP',
+        names: ['Egg plant', "Aubergine"],
+        colors: {}
+    },
+['Brussel sprouts'],
+['Cabbage', 'Savoy cabbage', 'Red cabbage', 'White cabbage', 'Sauerkraut'],
+['Carrot'],
+['Cassava'],
+['Cauliflower'],
+['Celery' ],
+['Chard'],
+['Collard', 'Spring greens'],
+['Corn', 'Sweetcorn', 'Corn on the cob', 'Baby sweetcorn'],
+['Zucchini', 'Courgette', 'Marrow'],
+['Cucumber', 'Gherkin', 'Pickle'],
+['Endive'],
+['Fennel'],
+['Fresh herbs', 'Basil', 'Parsley', 'Cilantro', 'Coriander' 'Mint', 'Dill'],
+['Garlic', 'Wild garlic'],
+['Ginger', 'Turmeric'],
+['Green beans', 'Runner beans', 'Fine beans', 'String beans', 'Broad beans', 'Edamame beans'],
+['Jerusalem artichoke' ],
+['Kale', 'Cavolo nero', 'Curly kale', 'Black kale', 'Purple kale'],
+['Kholerabi'],
+['Leeks'],
+['Lettuce', 'Salad leaves', 'Chicory', 'Romain lettuce', 'Iceberg'],
+['Mushrooms'],
+['Olives'],
+['Okra'],
+['Onions', 'Chalots'],
+['Pak choi', 'Bok choi'],
+['Parsnips '],
+['Peas', 'Mange tout', 'Petit pois', 'Snap pea'],
+['Pepper', 'Bell pepper', 'Romano pepper', 'Chile', 'Capsicums'],
+['Potato'],
+['Raddish', 'Daikon'],
+['Rhubarb'],
+['Spinach'],
+['Spring onions', 'Chives'],
+['Squash', 'Pumpkin', 'Butternut squash'],
+['Swede', 'Turnip', 'Rutabaga', 'Neep'],
+['Sweet potato'],
+['Tomato'],
+['Watercress'],
+['Water chestnuts'],
+['Yam'],
+];
