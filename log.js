b7c2806334c02f84ae3d8f0c5787f028f88f6df6
Pete Davis <7900180+petedavisdev@users.noreply.github.com>
2020-09-09 19:47:12 +0100

    Update log.js

--- log.js
+++ log.js
@@ -25,10 +25,12 @@ const myData = {
 const year2020week36 = {
     20200902: ["Avo", "Cab", "Car", "Cuc", "Gar", "GrB", "Lee", "Oni", "Tom"], // 9
     20200903: ["Cab", "Gar", "GrB", "Oni", "Tom"], // 5
-    20200904: ["Bea", "Cab", "Cuc", "Gar", "Oni", "Tom"], // 6
+    20200904: ["Bea", "Cab", "Cuc", "Gar", "Oni", "Tom", "Spr"], // 7
     20200905: ["Bea", "Cab", "Car", "Gar", "Oni", "Pea", "Tom"], // 7
     20200906: ["Bea", "Cab", "Car", "Gar", "Grb", "Mus", "Oni", "Pot", "Tom"], // 9
 };
 const year2020week37 = {
     20200907: ["Bea", "Cab", "Car", "Cou", "Gar", "Oni", "Pea", "Tom"], // 8
+    20200908: ["Cab", "Car", "Gar", "Oni", "Tom"], // 5
+    20200909: ["Cuc", "GrB", "Mus", "Oni" "Pep", "Tom"], // 6
 };
