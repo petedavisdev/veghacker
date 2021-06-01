56f08ed988759d4e4c66f22f79060600b9224dad
Pete Davis <pete.davis@rcn.org.uk>
2021-06-01 10:59:46 +0100

    Service Worker

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import './modern-normalize.css'
 import './index.css'
 import { router } from './router'
 
+if ('serviceWorker' in navigator) {
+  navigator
+    .serviceWorker
+    .register('service-worker.js')
+    .then(() => console.log('Registration Successful'))
+}
+
 const app = createApp(App)
 
 app.use(router)
