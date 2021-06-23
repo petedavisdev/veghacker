adb4e8d2996a5826cdd229d735a5301fcedf7015
Pete Davis <oldmanplanet@gmail.com>
2021-06-23 21:09:39 +0100

    update auth form

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,22 +1,31 @@
 <template>
-  <main class="w-full sm:w-1/2 xl:w-1/3">
-    <div class="border-teal p-8 border-t-12 mb-6 rounded-lg shadow-lg bg-white">
-      <div class="mb-4">
+    <form>
         <label class="font-bold text-grey-darker block mb-2">Email</label>
         <input
-          type="text"
-          class="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
-          placeholder="Your email"
-          v-model="email"
+            type="text"
+            class="
+                block
+                appearance-none
+                w-full
+                bg-white
+                border border-grey-light
+                hover:border-grey
+                px-2
+                py-2
+                rounded
+                shadow
+            "
+            placeholder="Your email"
+            v-model="email"
         />
-      </div>
-      <div class="flex flex-col gap-2">
-        <a @click="handleLogin({ email })" href="#" class="btn-black-outline">
-          Send me a magic link
-        </a>
-      </div>
-    </div>
-  </main>
+        <label>
+            <input type="checkbox" required />
+            As and early veghacker, I am happy to be asked for feedback and I
+            can tolerate a few bugs!
+        </label>
+
+        <button type="submit">Send me a magic link</button>
+    </form>
 </template>
 
 <script lang="ts">
@@ -24,15 +33,15 @@ import { defineComponent, ref } from "vue";
 import { handleLogin } from "../vuetils/useAuth";
 
 export default defineComponent({
-  name: "Auth",
-  setup() {
-    const email = ref("");
+    name: "Auth",
+    setup() {
+        const email = ref("");
 
-    return {
-      email,
-      handleLogin
-    };
-  }
+        return {
+            email,
+            handleLogin,
+        };
+    },
 });
 </script>
 
