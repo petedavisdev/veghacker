1c0df2cbb08c12aaaea29781f62345089de99a6c
petedavisdev <oldmanplanet@gmail.com>
2021-08-22 18:31:52 +0100

    supabase setup

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,42 @@
 <template>
-  <header>
-    <router-link to="/">
-      Veghacker v0.1-alpha
-    </router-link>
+	<header>
+		<router-link to="/"> Veghacker v0.1-alpha </router-link>
 
-    <nav>
-      <a href="/about">
-        About
-      </a>
-    </nav>
-  </header>
+		<nav>
+			<a href="/about"> About </a>
+		</nav>
+	</header>
 
-  <router-view />
+	<router-view />
 </template>
+
+<script lang="ts">
+import { Session } from "@supabase/supabase-js";
+import { defineComponent, ref } from "vue";
+import { supabase } from "./supabase";
+
+export default defineComponent({
+	setup() {
+		const userSession = ref<Session | null>(null);
+
+		supabase.auth.onAuthStateChange((event, session) => {
+			userSession.value = session;
+		});
+	},
+});
+</script>
+
 
 <style scoped>
 a {
-  text-decoration: none;
-  font-size: small;
+	text-decoration: none;
+	font-size: small;
 }
 
 header {
-  display: flex;
-  justify-content: space-between;
-  padding: 1em;
-  background-color: #124;
+	display: flex;
+	justify-content: space-between;
+	padding: 1em;
+	background-color: #124;
 }
 </style>
--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,42 +1,71 @@
 <template>
-    <h1>Account</h1>
+	<h1>Account</h1>
 
+	<form v-if="!userSession" @submit.prevent="login">
+		<p>
+			<label>
+				Email
+				<input v-model="state.email" type="email" required />
+			</label>
+		</p>
 
-    <form @submit.prevent="login">
-        <label for="email">
-            Email
-        </label>
+		<p>
+			<label>
+				<input type="checkbox" required />
+				As and alpha veghacker, I am happy to be asked for feedback and
+				I can tolerate a few bugs!
+			</label>
+		</p>
 
-        <input
-            id="email"
-            type="email"
-            v-model="state.email"
-            required
-        />
+		<button type="submit">▷</button>
+	</form>
 
-        <button type="submit">
-            ▷
-        </button>
-    </form>
+	<template v-else>
+		<Suspense>
+			<template #default>
+				<p>
+					You are logged in as
+					{{ userSession.user }}
+				</p>
+			</template>
+
+			<template #fallback>
+				<p>Loading...</p>
+			</template>
+		</Suspense>
+	</template>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from 'vue'
+import { defineComponent, ref } from "vue";
+import { supabase } from "../supabase";
 
 export default defineComponent({
-    setup() {
-        const state = reactive({
-            email: ""
-        })
+	props: {
+		userSession: Object,
+	},
+	setup() {
+		const email = ref("");
 
-        function login() {
-            console.log(state.email)
-        }
+		async function login() {
+			try {
+				const { error } = await supabase.auth.signIn({
+					email: email.value,
+				});
 
-        return {
-            state,
-            login
-        }
-    },
-})
+				if (error) return alert("Error logging in: " + error.message);
+
+				alert("I've emailed you a magic login link!");
+			} catch (error) {
+				console.error("Error thrown:", error.message);
+				return alert(error.error_description || error);
+			}
+		}
+
+		return {
+			email,
+			login,
+		};
+	},
+});
 </script>
