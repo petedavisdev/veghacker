de66964a66123f25c20277f3f34bf56a25869aa0
petedavisdev <oldmanplanet@gmail.com>
2021-08-22 15:09:48 +0100

    Setup account page and login html

---
+++ src/views/Account.vue
@@ -0,0 +1,42 @@
+<template>
+    <h1>Account</h1>
+
+
+    <form @submit.prevent="login">
+        <label for="email">
+            Email
+        </label>
+
+        <input
+            id="email"
+            type="email"
+            v-model="state.email"
+            required
+        />
+
+        <button type="submit">
+            ▷
+        </button>
+    </form>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from 'vue'
+
+export default defineComponent({
+    setup() {
+        const state = reactive({
+            email: ""
+        })
+
+        function login() {
+            console.log(state.email)
+        }
+
+        return {
+            state,
+            login
+        }
+    },
+})
+</script>
